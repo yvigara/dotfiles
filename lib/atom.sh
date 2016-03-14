b9dcacc4936241d1a82fbb1712f7ac7ab258ac66
Yann Vigara <yann.vigara@net-a-porter.com>
2016-03-14 12:27:05 +0000

    Add Atom plugin installation

---
+++ lib/atom.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+apm install project-manager
+apm install vim-mode-plus
+apm install file-icons
+apm install linter # https:// atom.io/packages/linter
+apm install gruvbox
+apm install git-control
+apm install merge-conflicts
+apm install clipboard-history
+apm install build
+apm install minimap
+apm install dash
+apm install minimap-git-diff
+
+# Golang
+apm install go-config
+apm install go-plus
+apm install go-rename
+apm install godef
+apm install go-oracle
+apm install autocomplete-go
+apm install go-outline
+# Ruby
+apm install autocomplete-ruby
+apm install linter-ruby
+#Python
+apm install autocomplete-python
+apm install python-tools
+apm install python-indent
+# JSON
+apm install jsonlint
+apm install pretty-json
+apm install colorful-json
+apm install autocomplete-json
+# Puppet
+apm install language-puppet
+apm install puppet-dark-syntax
+apm install linter-puppet
+apm install linter-puppet-parser
+apm install aligner-puppet
+
+
+
+
