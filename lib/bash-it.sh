ba91c34fada8e591f701c3077cfe425348d1d2d8
Yann Vigara <yann.vigara@paddypowerbetfair.com>
2016-04-25 16:55:46 +0100

    Enable default bash-it aliases/plugins/completion

---
+++ lib/bash-it.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash-it enable plugin ssh git base docker history osx xterm
+bash-it enable alias ansible atom docker general git homebrew homebrew-cask osx vagrant vim
+bash-it enable completion bash-it brew defaults dirs docker git git_flow packer ssh terraform vagrant vagrant
