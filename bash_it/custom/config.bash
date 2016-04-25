f4a1cd7b5905393133f299c85b53637e9cd5a93c
Yann Vigara <vigaray@iham16851.sportex.com>
2016-04-25 16:53:51 +0100

    Remove rerun specific

--- bash_it/custom/config.bash
+++ bash_it/custom/config.bash
@@ -1,9 +1,6 @@
-export GO15VENDOREXPERIMENT=1
 export GOPATH=$HOME/golang
 export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
-#export PATH=$PATH:$GOROOT/bin
-#export `gb info|sed -e 's/"//g'`
 ~/.vim/plugged/gruvbox/gruvbox_256palette_osx.sh
 
 HISTSIZE=999999
@@ -17,9 +14,3 @@ LESS="-R"
 
 export EDITOR VISUAL PAGER LESS
 export CLICOLOR=1
-
-export RERUN_HOME=$HOME/projects/puppet-rerun
-export PATH=$PATH:$RERUN_HOME
-export RERUN_MODULES=$RERUN_HOME/modules
-[ -t 0 ] && export RERUN_COLOR=true
-[ -r $RERUN_HOME/etc/bash_completion.sh ] && source $RERUN_HOME/etc/bash_completion.sh
