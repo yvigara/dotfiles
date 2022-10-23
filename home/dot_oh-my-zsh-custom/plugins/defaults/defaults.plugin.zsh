9793c68754c4eb709b259c73d3211d8aa01d4ac0
Yann Vigara <yvigara@gmail.com>
2022-10-23 14:32:32 +0100

    Improve ZSH setup

--- home/dot_oh-my-zsh-custom/plugins/defaults/defaults.plugin.zsh
+++ home/dot_oh-my-zsh-custom/plugins/defaults/defaults.plugin.zsh
@@ -1,28 +1,9 @@
 # -*- mode:sh; -*-
 ###############################################################################
 export GOPATH=$HOME/golang
-export GOROOT=/usr/local/opt/go/libexec
+export GOROOT=$(brew --prefix)/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
-VISUAL=vim
-EDITOR=vim
-PAGER="less -R"
-LESS="-R"
-alias vi="nvim"
-alias vim="nvim"
-alias vimdiff="nvim -d"
-
-export EDITOR VISUAL PAGER LESS
+export PAGER="less -R"
+export LESS="-R"
 export CLICOLOR=1
 
-if [ -d "$HOME/bin" ]; then
-  export PATH="$PATH:$HOME/bin"
-fi
-
-fpath=('/usr/local/share/zsh-completions' $fpath)
-[[ ! -f /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]] || source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-if [ -n "$(which direnv)" ]; then
-  eval "$(direnv hook zsh)"
-fi
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
